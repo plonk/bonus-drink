a6d2407b098118c7e07bcdcb48017fc1bdf6a234
Yoteichi <plonk@piano.email.ne.jp>
2014-04-28 08:59:46 +0900

    解いた

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,16 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    total = 0
+    bottles = 0
+    while amount > 0
+      total += 1
+      bottles += 1
+      if bottles == 3
+        amount += 1
+        bottles -= 3
+      end
+      amount -= 1
+    end
+    total
   end
 end
