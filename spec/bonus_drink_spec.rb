e6010a5dfac757bc4b3396ecfb84a4d22c6b0bf2
Yoteichi <plonk@piano.email.ne.jp>
2014-04-28 12:02:29 +0900

    add some more tests

--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -6,4 +6,7 @@ describe BonusDrink do
   specify { expect(BonusDrink.total_count_for(3)).to eq 4 }
   specify { expect(BonusDrink.total_count_for(11)).to eq 16 }
   specify { expect(BonusDrink.total_count_for(100)).to eq 149 }
+  specify { expect(BonusDrink.total_count_for(9998)).to eq 14996 }
+  specify { expect(BonusDrink.total_count_for(9999)).to eq 14998 }
+  specify { expect(BonusDrink.total_count_for(10000)).to eq 14999 }
 end
