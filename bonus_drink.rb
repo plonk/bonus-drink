a2ae8dccd591cd8913a55454c90b2ae7fc051a34
Yoteichi <plonk@piano.email.ne.jp>
2014-04-28 12:03:29 +0900

    一般項ってやつなのかな

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,9 +1,11 @@
 class BonusDrink
   def self.total_count_for(amount)
-    if amount < 3
-      amount
-    else
-      3 + BonusDrink.total_count_for(amount - 2)
+    if amount == 0
+      0
+    elsif amount.even?
+      (amount - 1) / 2 * 3 + 2
+    elsif amount.odd?
+      amount / 2 * 3 + 1
     end
   end
 end
